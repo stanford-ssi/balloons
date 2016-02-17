b51dd44bdde4cbe35f15dc7d431f9e73d8008062
Andrew <milichab@gmail.com>
2016-02-16 21:01:49 -0800

    Raw gps changes
    Baud

--- Gen4/gps_raw/gps_raw.ino
+++ Gen4/gps_raw/gps_raw.ino
@@ -2,13 +2,16 @@
 
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(9600); 
+  Serial.begin(115200); 
   gps.begin(9600); 
+  pinMode(6, OUTPUT); 
 }
+
 
 void loop() {
   // put your main code here, to run repeatedly:
   while(gps.available()){
     Serial.print(gps.read()); 
   }
+  digitalWrite(6, LOW); 
 }
