679a6dc0d4efe67c44c940e317ce12aa931b91a6
Davy Ragland <dragland@stanford.edu>
2017-02-07 13:49:40 -0800

    Added config.

---
+++ VALBAL/src/main.cpp
@@ -0,0 +1,38 @@
+/*
+  Stanford Student Space Initiative
+  Balloons | VALBAL | January 2017
+  Davy Ragland | dragland@stanford.edu
+  Ella Hofmann-Coyle | ellahofm@stanford.edu
+  Jesus Cervantes | cerjesus@stanford.edu
+  Michal Adamkiewicz | mikadam@stanford.edu
+  Claire Huang | chuang20@stanford.edu
+  Matthew Tan | mratan@stanford.edu
+
+  File: main.cpp
+  --------------------------
+  Flight code for main HABEES avionics.
+*/
+
+// #include "Avionics.h"
+//
+// /***********************************  BOOT  ***********************************/
+// Avionics flightController;
+int main(void) {
+//   flightController.init();
+// /***********************************  MAIN  ***********************************/
+//   while(true) {
+//     flightController.updateData();
+//     flightController.evaluateState();
+//     flightController.sendComms();
+//     flightController.sleep();
+//   }
+}
+// /*********************************  CALLBACK  *********************************/
+// bool ISBDCallback() {
+//   if (flightController.finishedSetup()) {
+//     flightController.updateData();
+//     flightController.evaluateState();
+//     flightController.sleep();
+//   }
+//   return true;
+// }
